53db35920ed4ca344910c8a5f3c1e2a6d60fb73a
Emiliano Polanco Baquerizo <emilianopolancobaquerizo@Emilianos-MacBook-Pro.local>
2022-05-16 15:39:26 +0200

    Fixins the stories router and refactoring GiftListSection test

--- src/components/giftListSection/GiftListSection.test.tsx
+++ src/components/giftListSection/GiftListSection.test.tsx
@@ -1,11 +1,72 @@
-import { render } from "@testing-library/react";
-import { giftListCardData } from "../../utils/apiData";
+import { render, screen } from "@testing-library/react";
+import GiftListCardInterface from "../../interfaces/GiftListCardInterface";
 import GiftListSection, { actionPlaceholder } from "./GiftListSection";
+import { faker } from "@faker-js/faker";
 
 describe("Given a GiftListSection component", () => {
   describe("When it receives a group of gift list card data", () => {
-    test("Then it should display the data in the cards", () => {
-      render(<GiftListSection cardsData={giftListCardData} />);
+    const cardsData: GiftListCardInterface[] = [
+      {
+        title: faker.company.bs(),
+        description: faker.lorem.lines(1),
+        etiquettes: [faker.word.noun(), faker.word.noun(), faker.word.noun()],
+      },
+      {
+        title: faker.company.bs(),
+        description: faker.lorem.lines(1),
+        etiquettes: [faker.word.noun(), faker.word.noun(), faker.word.noun()],
+      },
+      {
+        title: faker.company.bs(),
+        description: faker.lorem.lines(1),
+        etiquettes: [
+          faker.word.noun(),
+          faker.word.noun(),
+          faker.word.noun(),
+          faker.word.noun(),
+        ],
+      },
+    ];
+    test("Then it should display all the headings in cards", () => {
+      render(<GiftListSection cardsData={cardsData} />);
+      const expectedHeadings = [
+        "Gift list",
+        ...cardsData.map((cardData) => cardData.title),
+      ];
+
+      const foundHeadings = screen.getAllByRole("heading");
+
+      expect(
+        foundHeadings.map((foundHeading) => foundHeading.textContent)
+      ).toEqual(expectedHeadings);
+    });
+
+    test("Then it should display all the descriptions in the cards", () => {
+      render(<GiftListSection cardsData={cardsData} />);
+
+      const foundDescription = screen.getByText(cardsData[0].description);
+      const foundDescription2 = screen.getByText(cardsData[1].description);
+      const foundDescription3 = screen.getByText(cardsData[2].description);
+
+      expect(foundDescription).toBeInTheDocument();
+      expect(foundDescription2).toBeInTheDocument();
+      expect(foundDescription3).toBeInTheDocument();
+    });
+
+    test("Then it should display the etiquettes in the cards", () => {
+      render(<GiftListSection cardsData={cardsData} />);
+      const expectedEtiquettes = [
+        ...cardsData[0].etiquettes,
+        ...cardsData[1].etiquettes,
+        ...cardsData[2].etiquettes,
+      ];
+
+      const foundListItems = screen.getAllByRole("listitem");
+      const foundEtiquettes = foundListItems
+        .map((foundEtiquette) => foundEtiquette.textContent)
+        .filter((foundListItem) => (foundListItem as string).length < 15);
+
+      expect(foundEtiquettes).toEqual(expectedEtiquettes);
     });
   });
 });
